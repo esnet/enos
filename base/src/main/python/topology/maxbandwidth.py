fcae498162750a23cf26e5e2b291a4951a2cb1c2
davidhua <dhua@berkeley.edu>
2014-07-09 14:48:34 -0700

    Updates to bidirectional metric checking to fix issues with multiple links; updates to maxBandwidth to rely on built-in Dijkstra.

--- base/src/main/python/topology/maxbandwidth.py
+++ base/src/main/python/topology/maxbandwidth.py
@@ -1,8 +1,8 @@
-from net.es.enos.api import ModifiedDijkstra
 from net.es.enos.api import TopologyFactory
 from net.es.enos.esnet import OSCARSReservations
 from net.es.enos.api import TopologyProvider
 from org.joda.time import DateTime
+from org.jgrapht.alg import DijkstraShortestPath
 
 # Create graph with bandwidth weights rather than network metrics
 topology = TopologyFactory.instance()
@@ -23,9 +23,7 @@ maxReservable = -1
 # Calculate max bandwidth possible from source to destination
 
 tgraph = topo.getGraph(start, end, TopologyProvider.WeightType.MaxBandwidth)
-
-md = ModifiedDijkstra(tgraph, srcNode, dstNode)
-maxBandwidth = md.getBandwidth()
+maxBandwidth = DijkstraShortestPath.findPathBetween(tgraph, srcNode, dstNode)
 
 # iterate through path to calculate what is the max bandwidth available and the path corresponding
 for link in maxBandwidth:
--- base/src/main/python/topology/ports.py
+++ base/src/main/python/topology/ports.py
@@ -1,5 +1,5 @@
 from net.es.enos.api import TopologyFactory
-
+from net.es.enos.api import TopologyProvider
 
 #
 # Created by davidhua on 6/13/14.
@@ -10,17 +10,23 @@ from net.es.enos.api import TopologyFactory
 # Copied over from path.py-- initializes and fetches topology and list of nodes
 topology = TopologyFactory.instance()
 topo = topology.retrieveTopologyProvider("localLayer2")
-graph = topo.retrieveTopology()
-nodes = topo.getNodes()
-nodesByLink = topo.getNodesByLink()
 links = topo.getPortsByLink()
-listOfLinks = topo.getLinks()
+listOfLinks = topo.getInternalLinks()
 linkset = listOfLinks.values()
+counter = 0
+
+completedList = {}
+
+graph = topo.getGraph(TopologyProvider.WeightType.TrafficEngineering)
 
 # Iterate through links
-for linkset in listOfLinks:
+for linked in graph.edgeSet():
+    #for linked in linkk:
+    forwardSuccess = 0
+    backwardSuccess = 0
+
     # Normalize links
-    linked = listOfLinks.get(linkset)
+
     linkSplit = linked.getId().split(":")
     if linkSplit[3] != "es.net":
         continue;
@@ -28,27 +34,59 @@ for linkset in listOfLinks:
     source = topo.getNode(sourceNode)
 
     # Get remote link and normalize
-    remoteSplit = listOfLinks.get(linkset).getRemoteLinkId().split(":");
+    remoteSplit = linked.getRemoteLinkId().split(":");
+
     if remoteSplit[3] != "es.net":
         continue;
     targetNode = remoteSplit[4] + "@" + remoteSplit[3]
+    #print remoteSplit[4]
     target = topo.getNode(targetNode)
 
+
     # Find forwards and backwards edge
-    forwards = graph.getEdge(source, target)
-    backwards = graph.getEdge(target, source)
+    forwards = graph.getAllEdges(source, target)
+    backwards = graph.getAllEdges(target, source)
+    if forwards.isEmpty():
+        continue
+    if backwards.isEmpty():
+        continue
 
+    for forward in forwards:
+        if forward.equals(linked):
+            forwardSuccess = 1
+            realForward = forward
+
+    if forwardSuccess == 1:
+        reverse = realForward.getRemoteLinkId()
+    else:
+        continue
+
+
+    for backward in backwards:
+        if backward.getId() == (reverse):
+            realBackward = backward
+            backwardSuccess = 1
+    if backwardSuccess != 1:
+        continue
+    forwards = realForward
+    backwards = realBackward
     # Skip invalid edges for now
-    if forwards is None:
+    if completedList.has_key(forwards):
         continue
-    if backwards is None:
+    completedList[forwards] = completedList.get(forwards, 1)
+
+    if completedList.has_key(backwards):
         continue
 
+    completedList[backwards] = completedList.get(backwards, 1)
+    counter+=1
     # Check to see if metrics are the same
     if forwards.getTrafficEngineeringMetric() != backwards.getTrafficEngineeringMetric():
         print sourceNode, " and ", targetNode, " metrics possibly do not match"
-
+        print forwards.getId(), forwards.getTrafficEngineeringMetric()
+        print backwards.getId(), backwards.getTrafficEngineeringMetric()
         #print ""
-        #
-        #if forwards.getInterfaceMTU() != backwards.getInterfaceMTU():
-        #print sourceNode, " and ", targetNode, " MTUs possibly do not match"
+
+    if forwards.getInterfaceMTU() != backwards.getInterfaceMTU():
+        print ""
+        print sourceNode, " and ", targetNode, " MTUs possibly do not match"
