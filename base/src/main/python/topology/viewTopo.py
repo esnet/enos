91ba686687e8b9827bea1096bbc198c4be8ff4d9
davidhua <dhua@berkeley.edu>
2014-07-22 15:29:20 -0700

    Add TopologyViewer and associated python file, which visualizes topology, currently only stores image to file-- plenty of bugs abound. Run by calling viewTopo.py without arguments.

---
+++ base/src/main/python/topology/viewTopo.py
@@ -0,0 +1,17 @@
+from net.es.enos.api import TopologyViewer
+from net.es.enos.api import TopologyFactory
+from net.es.enos.api import TopologyProvider
+
+#
+# Created by davidhua on 7/17/14.
+#
+
+# Run by calling viewtopo.py in enos (No arguments).
+
+topology = TopologyFactory.instance()
+topo = topology.retrieveTopologyProvider("localLayer2")
+graph = topo.getGraph(TopologyProvider.WeightType.TrafficEngineering)
+
+topo = TopologyViewer(graph)
+topo.init()
+
