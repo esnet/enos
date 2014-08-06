efdb44588aa6f0c2ea6058443bc569c8fe1449dc
lomaxfrog <lomaxfrog@yahoo.com>
2014-08-05 17:24:13 -0700

    - fix a bug in BootStrap causing an inconsistent name when ENOS Root is a directory that is itself sym-linked # Please enter the commit message for your changes. Lines starting # with '#' will be ignored, and an empty message aborts the commit. # On branch master # Changes to be committed: #   (use "git reset HEAD <file>..." to unstage) # #	modified:   src/main/java/net/es/enos/kernel/container/Container.java #	modified:   src/main/java/net/es/enos/kernel/container/ContainerACL.java #	modified:   src/main/java/net/es/enos/kernel/container/Containers.java #	modified:   src/main/java/net/es/enos/kernel/exec/KernelThread.java #	modified:   src/main/java/net/es/enos/kernel/security/KernelSecurityManager.java #	modified:   src/main/java/net/es/enos/shell/ContainerShellCommands.java #	modified:   src/main/java/net/es/enos/shell/Shell.java # # Untracked files: #   (use "git add <file>..." to include in what will be committed) # #	.idea/ #	.name #	cachedir/ #	compiler.xml #	copyright/ #	encodings.xml #	enos.iml #	entmp/ #	etc/ #	hostkey.ser #	jyphon-cachedir/ #	libraries/ #	main/ #	misc.xml #	modules.xml #	scopes/ #	src.old/ #	target/ #	tmp/ #	topologies/ #	uiDesigner.xml #	users/ #	vcs.xml #	workspace.xml

--- base/src/main/java/net/es/enos/boot/BootStrap.java
+++ base/src/main/java/net/es/enos/boot/BootStrap.java
@@ -1,37 +1,15 @@
 /*
- * Copyright (c) 2014, Regents of the University of California.
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- * 1. Redistributions of source code must retain the above copyright notice,
- * this list of conditions and the following disclaimer.
- *
- * 2. Redistributions in binary form must reproduce the above copyright notice,
- * this list of conditions and the following disclaimer in the documentation
- * and/or other materials provided with the distribution.
- *
- * 3. Neither the name of the copyright holder nor the names of its contributors
- * may be used to endorse or promote products derived from this software without
- * specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * Copyright (c) 2014, Regents of the University of California  All rights reserved.
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+ * 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
 package net.es.enos.boot;
 
 import net.es.enos.api.DefaultValues;
-
 import net.es.enos.api.ENOSException;
 import net.es.enos.configuration.ENOSConfiguration;
 import net.es.enos.configuration.GlobalConfiguration;
@@ -41,20 +19,19 @@ import net.es.enos.kernel.security.KernelSecurityManager;
 import net.es.enos.python.PythonShell;
 import net.es.enos.shell.*;
 import net.es.enos.sshd.SShd;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.impl.SimpleLogger;
 
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-import org.slf4j.LoggerFactory;
-import org.slf4j.Logger;
-import org.slf4j.impl.SimpleLogger;
-
 /**
  * Created by lomax on 2/20/14.
  */
-public class BootStrap implements Runnable {
+public final class BootStrap implements Runnable {
     private static BootStrap bootStrap = null;
     private String[] args = null;
     private SShd sshd = null;
@@ -65,12 +42,25 @@ public class BootStrap implements Runnable {
     private static final GlobalConfiguration masterConfiguration = ENOSConfiguration.getInstance().getGlobal();
     private static final KernelSecurityManager securityManager = new KernelSecurityManager();
 
-    public final static Path rootPath = Paths.get(
-            masterConfiguration.getRootDirectory() != null ?
-                    masterConfiguration.getRootDirectory() :
-                    DefaultValues.ENOS_DEFAULT_ROOTDIR).normalize();
+    public final static Path rootPath = BootStrap.toRootRealPath();
 
     final private Logger logger = LoggerFactory.getLogger(BootStrap.class);
+
+    static public Path toRootRealPath() {
+
+        Path realPathName;
+        try {
+            if (masterConfiguration.getRootDirectory() != null) {
+                realPathName = Paths.get(
+                        new File(masterConfiguration.getRootDirectory()).getCanonicalFile().toString());
+            } else {
+                realPathName = Paths.get(new File(DefaultValues.ENOS_DEFAULT_ROOTDIR).getCanonicalFile().toString());
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e.getMessage());
+        }
+        return realPathName;
+    }
 
     public Shell getShell() {
         return shell;
