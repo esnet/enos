cac4d1f634755b5ae9262c187ff711cac7b9e09d
Bruce A. Mah <bmah@es.net>
2014-08-07 13:33:03 -0700

    Commit test / sample code for reading from sLS and esmond.

---
+++ base/src/main/python/perfsonar/read-esmond-data.py
@@ -0,0 +1,78 @@
+from net.es.enos.api import EsmondClient
+from net.es.enos.api import EsmondMeasurementFilter
+from net.es.enos.api import EsmondDataFilter
+
+# Configuration
+baseUrl = "http://lbl-pt1.es.net:9085/esmond/perfsonar/archive"
+timeRange=86400
+
+# Retrieve measurements from esmond
+c = EsmondClient()
+f = EsmondMeasurementFilter()
+
+# Retrieve all measurement sets and print them
+measurements = c.retrieveEsmondMeasurements(baseUrl, f)
+print "=== measurements on " + baseUrl + " ==="
+for m in measurements:
+    print m.getToolName(), m.getSource(), m.getDestination()
+    for e in m.getEventTypes():
+        print " ", e.getEventType(), e.getBaseUri()
+
+print "==="
+
+# Now find iperf3 measurements
+df = EsmondDataFilter()
+df.setTimeRange(timeRange)
+print "=== iperf3 measurements ==="
+for m in measurements:
+    if m.getToolName() == "bwctl/iperf3":
+        print m.getToolName(), m.getSource(), m.getDestination()
+        for e in m.getEventTypes():
+            if e.getEventType() == "throughput":
+                ds = m.retrieveData(e, df)
+                print "---"
+                for d in ds:
+                    print d.getTs(), d.getVal()
+                    
+                print "---"
+                
+print "==="
+
+# Now find traceroute measurements
+df = EsmondDataFilter()
+df.setTimeRange(timeRange)
+print "=== traceroute measurements ==="
+for m in measurements:
+    if m.getToolName() == "bwctl/tracepath,traceroute":
+        print m.getToolName(), m.getSource(), m.getDestination()
+        for e in m.getEventTypes():
+            if e.getEventType() == "packet-trace":
+                ds = m.retrieveData(e, df)
+                print "---", len(ds), " paths ---"
+                for d in ds:
+                    print d.getTs(), len(d.getVal()), "points"
+                    for p in d.getVal():
+                        print "  ", p.getTtl(), p.getIp(), p.getRtt()
+                    
+                print "---"
+                
+print "==="
+
+# Finally...owamp measurements
+
+df = EsmondDataFilter()
+df.setTimeRange(timeRange)
+print "=== owamp measurements ==="
+for m in measurements:
+    if m.getToolName() == "powstream":
+        print m.getToolName(), m.getSource(), m.getDestination()
+        for e in m.getEventTypes():
+            if e.getEventType() == "histogram-owdelay":
+                ds = m.retrieveData(e, df)
+                print "---", len(ds), " samples ---"
+                for d in ds:
+                    print d.getTs(), len(d.getVal()), "buckets", d.getVal()
+                    
+                print "---"
+                
+print "==="
---
+++ base/src/main/python/perfsonar/read-psmetadata.py
@@ -0,0 +1,24 @@
+from net.es.enos.api import SimpleLookupService
+from net.es.lookup.queries.Network import PSMetadataQuery
+
+# Query sls to get stuff
+
+# Instantiate sLS client
+sls = SimpleLookupService()
+
+# Retrieve all hosts in es.net domain
+hosts = sls.retrieveHostsByDomain("es.net")
+
+print "=== hosts ==="
+for h in hosts:
+    print h.getId()
+    for i in h.getInterfaces():
+        print "   ", i.getIfName(), i.getAddresses()
+
+# Try to get psmetadata records
+q = PSMetadataQuery()
+psms = sls.queryPSMetadata(q)
+print "=== psmetadata URIs ==="
+for psm in psms:
+    print psm.getPsmUri()
+
