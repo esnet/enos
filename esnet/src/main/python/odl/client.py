d96d9d8de5e58795ffefd6057fc570ea4e370a5d
Bruce A. Mah <bmah@es.net>
2015-04-03 12:02:14 -0700

    Checkpoint commit...starting to work on the interface between the testbed code and the OpenDaylight client code in Java-land.
    Adds the netshell-odl module as a dependency in the POM so we can get visibility into the net.es.netshell.odl classes.

--- esnet/src/main/python/odl/client.py
+++ esnet/src/main/python/odl/client.py
@@ -1,7 +1,31 @@
+from java.util import LinkedList
 
-class ODLClient(object):
+from common.openflow import SimpleController
+
+from org.opendaylight.controller.sal.core import Node
+
+import net.es.netshell.odl.Controller
+
+from org.opendaylight.controller.sal.match import Match
+from org.opendaylight.controller.sal.match import MatchType
+
+from org.opendaylight.controller.sal.action import SetDlDst
+from org.opendaylight.controller.sal.action import SetDlSrc
+from org.opendaylight.controller.sal.action import PopVlan
+from org.opendaylight.controller.sal.action import PushVlan
+from org.opendaylight.controller.sal.action import Output
+
+from org.opendaylight.controller.sal.flowprogrammer import Flow
+
+
+class ODLClient(SimpleController):
+    """
+    Class that is an interface to the ENOS OpenDaylight client.
+    The real client functionality is the net.es.netshell.odl.Controller
+    class (in Java).
+    """
+
     _instance = None
-
 
     def __new__(cls, *args, **kwargs):
         """
@@ -10,3 +34,82 @@ class ODLClient(object):
         if not cls._instance:
             cls._instance = super(ODLClient, cls).__new__(cls, *args, **kwargs)
         return cls._instance
+
+    def __init__(self):
+        SimpleController.__init__(self)
+        self.odlController = net.es.netshell.odl.Controller.getInstance()
+
+    def addFlowMod(self, flowMod):
+        """
+        Implementation of addFlowMod for use with OpenDaylight.
+        Uses the net.es.netshell.odl.Controller.
+        :param flowMod:
+        :return:
+        """
+        # check scope
+        if flowMod.scope.isValidFlowMod(flowMod):
+
+            # Find switch
+            # XXX I bet this operation is expensive.  Maybe we should think about putting in
+            # a one-deep cache of the switch lookup.
+            switches = self.odlController.getNetworkDevices()
+            sw = None
+            for s in switches:
+                # Find the switch that has the same DPID as the one we want to talk to.
+                # Note that we also have the mininet switch name in flowMod.switch.props['mininetName']
+                if s.dataLayerAddress == flowMod.switch.props['dpid']: # Representation of DPID?
+                    sw = s
+                    break
+            if sw == None:
+                return False
+
+            # Compose match object
+            match = Match()
+
+            val = flowMod.props['match'].props['in_port']
+            if val != None:
+                match.setField(MatchType.IN_PORT, val)
+            val = flowMod.props['match'].props['dl_src']
+            if val != None:
+                match.setField(MatchType.DL_SRC, val)
+            val = flowMod.props['match'].props['dl_dst']
+            if val != None:
+                match.setField(MatchType.DL_DST, val)
+            val = flowMod.props['match'].props['vlan']
+            if val != None:
+                match.setField(MatchType.DL_VLAN, val)
+
+            # Compose action.
+            # We do the data-link and VLAN translations first.  Other types of
+            # translations would happen here as well.  Then any action to forward
+            # packets.
+            actionList = LinkedList()
+
+            val = flowMod.props['actions'].props['dl_dst']
+            if val != None:
+                actionList.add(SetDlDst(val))
+            val = flowMod.props['actions'].props['dl_src']
+            if val != None:
+                actionList.add(SetDlSrc(val))
+            val = flowMod.props['actions'].props['vlan']
+            if val != None:
+                actionList.add(PopVlan())
+                actionList.add(PushVlan(val))
+            val = flowMod.props['actions'].props['out_port'] # how to parse this
+            if val != None:
+                # Iterate over out_port values and add to actionList
+
+            # compose flow
+            flow = Flow(match, actionList)
+
+            # go to the controller
+            self.odlController.addFlow(sw.node, flow)
+
+            # get result
+            return True
+
+        return False
+
+    def delFlowMod(self, flowMod):
+
+        return False
