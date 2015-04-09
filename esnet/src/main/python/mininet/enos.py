137d5d5a41c3fc54ed185c240b4f6826f03b1319
Bruce A. Mah <bmah@es.net>
2015-04-09 11:54:24 -0700

    Checkpoint:  Towards getting virtual circuits in OpenFlow.
    We can compute the router ports correctly.  Computing the flow mods
    and pushing them to the controller has been mostly implemented
    but not tested yet.

--- esnet/src/main/python/mininet/enos.py
+++ esnet/src/main/python/mininet/enos.py
@@ -4,6 +4,7 @@ import sys
 from mininet.testbed import TopoBuilder
 from net.es.netshell.api import GenericTopologyProvider, TopologyProvider, GenericHost, GenericNode, GenericPort, GenericLink
 from common.api import Properties
+from common.openflow import Match, Action, FlowMod, Scope
 from odl.client import ODLClient
 
 nodes = {}
@@ -102,6 +103,104 @@ class TestbedTopology (GenericTopologyProvider):
         for vpnName in self.builder.vpns:
             vpn = self.builder.vpns[vpnName]
             self.buildVpn(vpn)
+
+    def findCoreRouterPorts(self, router, target):
+        """
+        Find the router ports facing a target
+        """
+        coreRouterLinks = []
+        siteRouterLinks = []
+        coreRouterPortLinks = {}
+
+        for p in router.props['ports']:
+            for p2 in target.props['ports']:
+                if router.props['ports'][p].props['link'] == target.props['ports'][p2].props['link']:
+                    coreRouterPortLinks[p] = router.props['ports'][p]
+        return coreRouterPortLinks
+
+    def selectPort(self, ports):
+        """
+        Select a port that we want to use from a set of ports.  Use this to
+        pick one from a set of ports going to parallel links.  For now
+        we'll prefer the lowest numbered ports and best-effort service.
+        """
+        for p in ports:
+            linkName = ports[p].props['link']
+            if linkName[-1:] == '1' or linkName[-12:] == ':best-effort':
+                return ports[p]
+        return None
+
+    def makeCircuitOnNode(self, controller, scope, router, startPort, endPort, startVlan, endVlan):
+        """
+        Set up the virtual circuit mapping on a router.  This is a bidirectional
+        circuit, so we need to insert two flow entries, one in each direction.
+        """
+        print "On router " + router.name + ":"
+        print startPort.name + " (" + startPort.props['link'] + ") VLAN " + str(startVlan) + " -> " + endPort.name + " (" + endPort.props['link'] + ") VLAN " + str(endVlan)
+        print endPort.name + " (" + endPort.props['link'] + ") VLAN " + str(endVlan) + " -> " + startPort.name + " (" + startPort.props['link'] + ") VLAN " + str(startVlan)
+
+        # If a controller is specified, push flows to it.
+        if controller:
+            m1 = Match()
+            m1.props['in_port'] = startPort.name
+            m1.props['vlan'] = startVlan
+            a1 = Action()
+            a1.props['vlan'] = endVlan
+            a1.props['out_port'] = endPort.name
+            f1 = FlowMod(scope, router, a1, m1)
+            controller.addFlowMod(f1)
+
+            m2 = Match()
+            m2.props['in_port'] = endPort.name
+            m2.props['vlan'] = endVlan
+            a2 = Action()
+            a2.props['vlan'] = startVlan
+            a2.props['out_port'] = startPort.name
+            f2 = FlowMod(scope, router, a2, m2)
+            controller.addFlowMod(f2)
+
+    def makeCircuit(self, controller, scope, startTarget, routers, endTarget, vlan):
+        """
+        Make a virtual circuit
+        """
+
+        router = routers[0]
+        startPorts = self.findCoreRouterPorts(router, startTarget)
+        endPort = None
+
+        if len(routers) == 1:
+            # Only one router in the path, so hook the start and end targets together
+            endPorts = self.findCoreRouterPorts(router, endTarget)
+        else:
+            # Multiple routers, so hook the start target to the router interface
+            # facing the next router in the path
+            endPorts = self.findCoreRouterPorts(router, routers[1])
+
+        startPort = self.selectPort(startPorts)
+        endPort = self.selectPort(endPorts)
+        self.makeCircuitOnNode(controller, scope, router, startPort, endPort, vlan, vlan)
+
+        # If there are more routers left to set up, call ourselves recursively
+        # to do the rest.  This is a cool form of tail recursion.
+        if len(routers) > 1:
+            self.makeCircuit(controller, scope, router, routers[1:], endTarget, vlan)
+
+
+    def makeCircuitNames(self, controller, scope, startTargetName, routerNames, endTargetName, vlan):
+        """
+        Make a virtual circuit given node names.
+        This function looks up the node names in the network model and feeds
+        the corresponding objects to makeCircuit.
+        """
+
+        startTarget = self.builder.nodes[startTargetName]
+        endTarget = self.builder.nodes[endTargetName]
+
+        routers = []
+        for r in routerNames:
+            routers.append(self.builder.coreRouters[r])
+
+        self.makeCircuit(controller, scope, startTarget, routers, endTarget, vlan)
 
     def __init__(self, fileName = None):
         # Build topology
