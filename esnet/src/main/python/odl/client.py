a5d1527f42c0b130f1f87da1b29564b3eccce114
Bruce A. Mah <bmah@es.net>
2015-04-16 13:07:16 -0700

    WIP checkpoint of PACKET_IN callback code.  In testing, we make it part way through parsing the packet and doing the ODL -> ENOS adaptation.

--- esnet/src/main/python/odl/client.py
+++ esnet/src/main/python/odl/client.py
@@ -1,6 +1,7 @@
 import struct
 from java.lang import Short
 from java.util import LinkedList
+from java.nio import ByteBuffer
 
 from common.openflow import SimpleController
 from common.utils import singleton
@@ -8,6 +9,9 @@ from common.utils import singleton
 from org.opendaylight.controller.sal.core import Node
 
 import net.es.netshell.odl.Controller
+import net.es.netshell.odl.PacketHandler
+
+from org.opendaylight.controller.sal.core import Node
 
 from org.opendaylight.controller.sal.match import Match
 from org.opendaylight.controller.sal.match.MatchType import DL_DST
@@ -15,14 +19,76 @@ from org.opendaylight.controller.sal.match.MatchType import DL_SRC
 from org.opendaylight.controller.sal.match.MatchType import DL_VLAN
 from org.opendaylight.controller.sal.match.MatchType import IN_PORT
 
-
 from org.opendaylight.controller.sal.action import Output
 from org.opendaylight.controller.sal.action import SetDlDst
 from org.opendaylight.controller.sal.action import SetDlSrc
 from org.opendaylight.controller.sal.action import PopVlan
 from org.opendaylight.controller.sal.action import PushVlan
 
+from org.opendaylight.controller.sal.packet import RawPacket
+from org.opendaylight.controller.sal.packet import PacketResult
+
 from org.opendaylight.controller.sal.flowprogrammer import Flow
+
+class ODLClientPacketInCallback(net.es.netshell.odl.PacketHandler.Callback):
+    """
+    Callback class to receive the result of PACKET_IN messages
+    """
+    def __init__(self, odlClient, testbed):
+        """
+
+        :param odlClient:  ODLClient object
+        :param testbed: TestbedTopology object, needed for figuring out ODL -> ENOS
+        :return:
+        """
+        self.odlClient = odlClient
+        self.odlController = odlClient.odlController
+        self.testbed = testbed
+
+    def callback(self, rawPacket):
+        """
+        And this is the callback itself.  Everything that touches an ODL-specific
+        data structure needs to go in here...above this layer it's all generic
+        OpenFlow.
+
+        :param rawPacket an instance of org.opendaylight.controller.sal.packet.RawPacket
+        """
+
+        print "Callback PACKET_IN"
+
+        # Find out where the packet came from (ingress port).  First find the node and connector
+        # in the ODL/Java world.
+        ingressConnector = rawPacket.getIncomingNodeConnector()
+        ingressNode = ingressConnector.getNode()
+
+        print "  from node type " + ingressNode.getType()
+
+        # Make sure this is an OpenFlow switch.  If not, ignore the packet.
+        if ingressNode.getType() == Node.NodeIDType.OPENFLOW:
+            print "  OpenFlow"
+
+            print "  ID is of type " + ingressNode.getID().__class__
+
+            # Get the dpid of the switch
+            bb = ByteBuffer.allocate(8)
+            dpid = bb.putLong(ingressNode.getID()).array()
+
+            print "  OpenFlow DPID " + dpid
+
+            # Need to get the ENOS/Python switch that corresponds to this DPID.
+            # mininet.testbed.TopoBuilder.dpidToName can get us the switch name.
+            switchName = testbed.builder.dpidToName[str(dpid)]
+
+            print "(" + switchName + ")"
+
+            return PacketResult.KEEP_PROCESSING
+
+        else:
+            print "  Non-OpenFlow"
+            return PacketResult.IGNORED
+
+        return PacketResult.KEEP_PROCESSING
+
 
 class ODLClient(SimpleController):
     """
@@ -37,6 +103,7 @@ class ODLClient(SimpleController):
         SimpleController.__init__(self)
         self.__class__ = ODLClient
         self.odlController = net.es.netshell.odl.Controller.getInstance()
+        self.packetHandler = net.es.netshell.odl.PacketHandler.getInstance()
 
     def makeODLFlowEntry(self, flowMod, odlNode):
         """
