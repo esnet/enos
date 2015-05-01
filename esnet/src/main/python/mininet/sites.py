e318dc056a56a94738e742d90fe67885296f7aa5
lomaxfrog <lomaxfrog@yahoo.com>
2015-05-01 16:04:12 -0700

    - remove debug print statement

--- esnet/src/main/python/mininet/sites.py
+++ esnet/src/main/python/mininet/sites.py
@@ -129,7 +129,6 @@ class SiteRenderer(ProvisioningRenderer,ScopeOwner):
         :param event: ScopeEvent
         """
         if event.__class__ == PacketInEvent:
-            print "#####", event
             # This is a PACKET_IN. Learn the source MAC address
             if not 'vlan' in event.props:
                 # no VLAN, reject
