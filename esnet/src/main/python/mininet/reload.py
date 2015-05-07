9c7eee7361697cd146f907d0f51789b2b83e4e62
Bruce A. Mah <bmah@es.net>
2015-05-07 13:05:15 -0700

    Add lomax@ reload script.
    This (along with some other scripts I've added recently to the mininet directory) should really go somewhere else.  At least this stuff is now under version control until we decide where that should be.

---
+++ esnet/src/main/python/mininet/reload.py
@@ -0,0 +1,26 @@
+from odl.client import ODLClient
+ODLClient(topology=None).stopCallback()
+
+import common.utils
+reload (common.utils)
+import common.api
+reload (common.api)
+import common.intent
+reload (common.intent)
+import common.openflow
+reload (common.openflow)
+import mininet.testbed
+reload (mininet.testbed)
+import mininet.enos
+reload (mininet.enos)
+import odl.client
+reload (odl.client)
+import mininet.l2vpn
+reload (mininet.l2vpn)
+import mininet.sites
+reload (mininet.sites)
+import mininet.wan
+reload (mininet.wan)
+
+
+
