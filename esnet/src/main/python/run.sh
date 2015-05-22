b3ad3e28a3072d42db07c05f8bc7c52cc74042c3
Bruce A. Mah <bmah@es.net>
2015-05-22 11:23:55 -0700

    Add a tiny script to make mininet startup easier.
    Run this as root to kick things off.

---
+++ esnet/src/main/python/run.sh
@@ -0,0 +1,9 @@
+#! /bin/sh
+#
+# Helper script for multi-point VPN mininet configuration and startup.
+# Invoke this as root to start things up and get a mininet
+# command-line prompt.
+#
+export PYTHONPATH=${PWD}
+python mininet/run.py
+
