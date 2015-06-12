ef0b03f2cf08964b30ac8485c3f3837c367dba26
rueiminl <RueiMinLin@lbl.gov>
2015-06-12 14:42:10 -0700

    implement utility dump() for checking props on object

--- esnet/src/main/python/mininet/utility.py
+++ esnet/src/main/python/mininet/utility.py
@@ -16,3 +16,22 @@ def InitLogger():
         
 def Logger():
     return logging.getLogger()
+
+def dump(obj):
+	print "dump " + str(obj.__class__) + ":", obj
+	for attr in dir(obj):
+		if attr[0:2] == '__':
+			continue
+		if attr == "props":
+			print "\t" + attr + ":"
+			for prop in obj.props.items():
+				if not isinstance(prop[1], dict) and not isinstance(prop[1], list):
+					print "\t\t" + prop[0] + str(prop[1].__class__), prop[1]
+				else:
+					print "\t\t" + prop[0] + str(prop[1].__class__)
+		else:
+			var = getattr(obj, attr)
+			if not isinstance(var, dict) and not isinstance(var, list):
+				print "\t" + attr + str(var.__class__), var
+			else:
+				print "\t" + attr + str(var.__class__)
