15570a715db25058a35bf91956adcfcd3e4d350e
lomaxfrog <lomaxfrog@yahoo.com>
2016-05-12 14:45:26 -0700

    - slicectl commands create-slice,delete-slice,list-slices,show-slice are now fully functional including persistency.

--- esnet/src/main/resources/Lib/layer2/testbed/slicectl.py
+++ esnet/src/main/resources/Lib/layer2/testbed/slicectl.py
@@ -40,7 +40,7 @@ def loadSlices():
     slices = Container.getContainer(Slices)
     allocatedSlices = slices.loadResources({"resourceType":ResourceTypes.SLICE})
     for slice in allocatedSlices:
-        multiplexerSlices[slice.getResourceName] = slice
+        multiplexerSlices[slice.getResourceName()] = slice
 
 
 """
@@ -69,33 +69,62 @@ def allocateNewID():
     slicerLock = globals()['slicerLock']
     with slicerLock:
         for id in range(1,255):
+            id = str(id)
             if id in multiplexerSlices:
                 continue
             return id
-        return 0
+        return None
 
 def createSlice(name,owner,id=None):
     multiplexerSlices = globals()['multiplexerSlices']
+    id = str(id)
     if id in multiplexerSlices:
         print "id ",id," exists already."
     else:
         id = allocateNewID()
-    slice = Resource(str(id))
+        if id == None:
+            print("cannot allocate new slice id")
+            return None
+    slice = Resource(id)
     slice.setDescription(name)
     slice.properties[SliceOwner] = owner
+    slice.setResourceType(ResourceTypes.SLICE)
     slices = Container.getContainer(Slices)
-    slices.save(slice)
+    slices.saveResource(slice)
     multiplexerSlices[id] = slice
-    return 0
+    return id
 
 def deleteSlice(id):
+    id = str(id)
     multiplexerSlices = globals()['multiplexerSlices']
-    if id in multiplexerSlices:
-        slices = Container.getContainer(Slices)
-        slices.deleteResource(id)
-    else:
-        print "slice ",id," does not exist"
+    slicerLock = globals()['slicerLock']
+    with slicerLock:
+        if id in multiplexerSlices:
+            slices = Container.getContainer(Slices)
+            slices.deleteResource(id)
+            del multiplexerSlices[id]
+        else:
+            print "slice ",id," does not exist"
 
+def displaySlices(owner=None):
+    multiplexerSlices = globals()['multiplexerSlices']
+    print "ID\tOwner\tDescription"
+    for (sliceId,slice) in multiplexerSlices.items():
+        if owner != None and slice[SliceOwner] != owner:
+            continue
+        print sliceId,"\t",slice.properties[SliceOwner],"\t",slice.getDescription()
+
+def showSlice(id):
+    id = str(id)
+    multiplexerSlices = globals()['multiplexerSlices']
+    if not id in multiplexerSlices:
+        print "slice id ",id," does not exists"
+        return
+    slice = multiplexerSlices[id]
+    print slice.toPrettyString()
+
+def getSlices():
+    return globals()['multiplexerSlices']
 
 def print_syntax():
     print "MP-VPN Multiplex Utility"
@@ -103,9 +132,10 @@ def print_syntax():
     print " Commands are:"
     print "\thelp"
     print "\t\tPrints this help."
-    print "\tslicectl create-slice <slice name> owner <user name> [id <id>]"
-    print "\tslicectl delete-slice <id>"
-    print "\tslicectl list-slices [grep <pattern>"
+    print "\tslicectl create-slice <slice name> owner <user name> [id <slice id>]"
+    print "\tslicectl delete-slice < slice id>"
+    print "\tslicectl list-slices [owner <usr name>]"
+    print "\tslicectl show-slice <slice id>"
     print "\tslicectl add-port <slice id> switch <switch name> port <port name> [vlan <VLAN>]"
     print "\tslicectl del-port <slice id> switch <switch name> port <port name> [vlan <VLAN>"
     print "\tslicectl list-ports <slice id> [switch <switch name>]"
@@ -127,13 +157,28 @@ def processCLI(argv):
         sliceName = argv[2]
         owner = argv[4]
         id = None
-        if len(argv) > 4:
+        if len(argv) > 5:
             id = argv[6]
         rid = createSlice(name = sliceName,owner=owner,id=id)
-        if (id < 0):
+        if (rid == None):
             print "failed to create slice"
             return
         print "slice created id= ", rid
+        return
+    elif cmd == "delete-slice":
+        id = argv[2]
+        deleteSlice(id)
+        return
+    elif cmd == "list-slices":
+        owner = None
+        if len(argv) > 2:
+            owner = argv[3]
+        displaySlices(owner=owner)
+        return
+    elif cmd == "show-slice":
+        id = argv[2]
+        showSlice(id)
+        return
     else:
         print "unknown command"
         print_syntax()
